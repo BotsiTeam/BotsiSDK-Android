9a1c2cc79c8453c82e2cc154d903c82811790cfa
bgMarkevych <chudik143@gmail.com>
2025-06-30 17:35:30 +0300

    small fix

--- botsi/src/main/kotlin/com/botsi/data/model/dto/BotsiUpdateProfileParametersDto.kt
+++ botsi/src/main/kotlin/com/botsi/data/model/dto/BotsiUpdateProfileParametersDto.kt
@@ -3,10 +3,10 @@ package com.botsi.data.model.dto
 import com.botsi.domain.model.BotsiProfile
 
 internal data class BotsiUpdateProfileParametersDto(
-    val birthday: String,
-    val email: String,
-    val userName: String,
-    val gender: String,
-    val phone: String,
+    val birthday: String?,
+    val email: String?,
+    val userName: String?,
+    val gender: String?,
+    val phone: String?,
     val custom: List<BotsiProfile.CustomEntry>
 )
