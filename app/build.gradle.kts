42de27efbd7e89d753336f31d91089fac47777ad
bgMarkevych <chudik143@gmail.com>
2025-07-05 11:41:32 +0300

    version code up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.botsi.example"
         minSdk = BotsiGlobalVars.minSdk
         targetSdk = BotsiGlobalVars.targetSdk
-        versionCode = 26
+        versionCode = 27
         versionName = "1.0"
         multiDexEnabled = true
     }
